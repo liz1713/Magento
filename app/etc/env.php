4720dde35937b1b263d86218cde87fdfc615ce26
liz1713 <giovannalizette@hotmail.com>
2020-02-12 17:21:28 -0700

    app completa

---
+++ app/etc/env.php
@@ -0,0 +1,71 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => '2cd7aa4eeb692263fb4319afa2de4eee'
+    ],
+    'db' => [
+        'table_prefix' => '',
+        'connection' => [
+            'default' => [
+                'host' => 'localhost',
+                'dbname' => 'database',
+                'username' => 'root',
+                'password' => 'root',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'default',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => 'f49_'
+            ],
+            'page_cache' => [
+                'id_prefix' => 'f49_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 1,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'google_product' => 1,
+        'full_page' => 1,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'downloadable_domains' => [
+        'localhost'
+    ],
+    'install' => [
+        'date' => 'Wed, 12 Feb 2020 23:04:51 +0000'
+    ]
+];
